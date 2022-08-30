1ab8aa05b2d009c583b4c40642c8d2332e04e5ce
DeleteAccess <lhaeller@tuta.io>
2022-08-30 18:05:37 +0200

    this order makes more sense

--- src/assets/portfolio-data.js
+++ src/assets/portfolio-data.js
@@ -1,32 +1,33 @@
-export const data = [{
-    name: 'Quality Monitoring App',
-    description: 'The app has a frontend and a backend version with a shared codebase. Key aspects were asynchronous calls, secure WebSocket connections and upholding data governance.',
-    techstack: ['JavaScript', 'HTML5', 'SCSS', 'Node.js', 'YAML'],
-    customer: 'at Siemens',
-    link: null
-},
+export const data = [
+    {
+        name: 'Full Stack Data App',
+        description: 'Visualization of KPI data from various data sources. I handled the full data-integration process and negotiated with data providers, sysadmins and customers alike.',
+        techstack: ['Python', 'PostgreSQL', 'Talend', 'Qlik Sense', 'AWS'],
+        customer: 'at Siemens',
+        link: null
+    },
+    
+    {
+        name: 'Quality Monitoring App',
+        description: 'The app has a frontend and a backend version with a shared codebase. Key aspects were asynchronous calls, secure WebSocket connections and upholding data governance.',
+        techstack: ['JavaScript', 'HTML5', 'SCSS', 'Node.js', 'YAML'],
+        customer: 'at Siemens',
+        link: null
+    },
 
-{
-    name: 'Full Stack Data App',
-    description: 'Visualization of KPI data from various data sources. I handled the full data-integration process and negotiated with data providers, sysadmins and customers alike.',
-    techstack: ['PostgreSQL', 'Talend', 'Qlik Sense', 'AWS'],
-    customer: 'at Siemens',
-    link: null
-},
+    {
+        name: 'Business Website',
+        description: 'I applied a user-centred design to build this website. Customizability, monitoring performance and upholding cybersecurity are key to the final product',
+        techstack: ['Wordpress', 'CSS', 'Google Ads', 'Google Analytics', 'Cloudflare'],
+        customer: 'for MBSR Zug',
+        link: 'https://www.mbsr-zug.ch'
+    },
 
-{
-    name: 'Business Website',
-    description: 'I applied a user-centred design to build this website. Customizability, monitoring performance and upholding cybersecurity are key to the final product',
-    techstack: ['Wordpress', 'CSS', 'Google Ads', 'Google Analytics', 'Cloudflare'],
-    customer: 'for MBSR Zug',
-    link: 'https://www.mbsr-zug.ch'
-},
-
-{
-    name: 'JS-based Desktop Game',
-    description: 'A competitive, time-based puzzle game in an 80s Arcade theme. I acted as lead programmer during this university project.',
-    techstack: ['JavaScript', 'HTML5', 'CSS3', 'YAML', 'Swagger', 'REST'],
-    customer: 'at FHGR',
-    link: 'https://github.com/DeleteAccess/point-destroyer'
-}
+    {
+        name: 'JS-based Desktop Game',
+        description: 'A competitive, time-based puzzle game in an 80s Arcade theme. I acted as lead programmer during this university project.',
+        techstack: ['JavaScript', 'HTML5', 'CSS3', 'YAML', 'Swagger', 'REST'],
+        customer: 'at FHGR',
+        link: 'https://github.com/DeleteAccess/point-destroyer'
+    }
 ]
